f4449112afaaf06b3cab773f05af7830e5e0318d
Felix Holz <felix.holz@me.com>
2024-10-07 12:26:08 +0200

    revert to basic remote funtionality

--- android/app/src/main/java/eu/ows/mosaicformobile/mosaic_for_mobile/MainActivity.java
+++ android/app/src/main/java/eu/ows/mosaicformobile/mosaic_for_mobile/MainActivity.java
@@ -3,22 +3,6 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
-
-
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.BM25Similarity;
-import org.apache.lucene.store.FSDirectory;
-
 public class MainActivity extends FlutterActivity {
 
     private static final String QUERY_CHANNEL = "ows.eu/search";
